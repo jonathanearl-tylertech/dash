7a1dfbaf5ee67f21b2cb07ce2af767c73344604c
jonathan earl <earl.jonathan@gmail.com>
2023-11-11 14:59:28 -0800

    seperate deployment secrets from app secrets

--- deployment/decrypt.sh
+++ deployment/decrypt.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
+set -e
 cd "$(dirname "$0")"
+
 export SOPS_AGE_KEY_FILE=~/.sops/age-key.txt
-export EDITOR=/usr/bin/nano
 sops -d config.enc.yaml > config.yaml
--- deployment/encrypt.sh
+++ deployment/encrypt.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 set -e
 cd "$(dirname "$0")"
+
 export SOPS_AGE_RECIPIENTS=$(<public-age-keys.txt)
 sops --encrypt --age ${SOPS_AGE_RECIPIENTS}  config.yaml > config.enc.yaml
