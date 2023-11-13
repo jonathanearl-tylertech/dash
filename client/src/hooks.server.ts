35148ad82bfca90e148331a2e7135616f409b526
jonathan earl <earl.jonathan@gmail.com>
2023-11-12 17:24:29 -0800

    fix(version): bind version to docker container

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -1,6 +1,10 @@
+import { env } from "$env/dynamic/private";
+import { logger } from "$lib/server/logger";
 import { useReadinessCheck } from "$lib/server/middleware/readiness-check";
 import { useUserAuthorization } from "$lib/server/middleware/user-authorization";
 import { sequence } from "@sveltejs/kit/hooks";
+
+logger.info({ VERSION: env.VERSION })
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = sequence(
