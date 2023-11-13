366d59533c75d3c03b0df26a2a896761c1ecfd90
jonathan earl <earl.jonathan@gmail.com>
2023-11-12 16:07:53 -0800

    fix(readiness): remove redundant readiness check

--- client/src/lib/server/middleware/readiness-check.ts
+++ client/src/lib/server/middleware/readiness-check.ts
@@ -1,7 +1,7 @@
 import { base } from "$app/paths";
 import type { Handle } from "@sveltejs/kit";
 
-export const readinessCheck: Handle = async ({ event, resolve }) => {
+export const useReadinessCheck: Handle = async ({ event, resolve }) => {
     if (event.url.pathname == `${base ?? ''}/health`)
         return new Response('healthy', { status: 200 })
     
